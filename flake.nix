058b8feb0e33a6df24e9c93d10c86cdfb1649fc0
Peter Jones <pjones@devalot.com>
2022-05-25 16:26:56 -0700

    Convert to Nix Flakes

---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Peter's Haskell Config Files and Scripts";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "armv7l-linux"
+        "aarch64-darwin"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "x86_64-linux"
+      ];
+
+      # Function to generate a set based on supported systems:
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+      # Attribute set of nixpkgs for each system:
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (system: {
+        default = nixpkgsFor.${system}.stdenvNoCC.mkDerivation {
+          name = "haskellrc";
+          src = ./.;
+          phases = [ "unpackPhase" "installPhase" "fixupPhase" ];
+
+          installPhase = ''
+            mkdir -p "$out/bin" "$out/etc"
+
+            for file in bin/*; do
+              install -m 0555 "$file" "$out/bin"
+            done
+
+            for file in etc/*; do
+              install -m 0444 "$file" "$out/etc"
+            done
+          '';
+        };
+      });
+
+      homeManagerModules.default = { pkgs, ... }: {
+        config = {
+          home.file = {
+            ".ghci".source =
+              "${self.packages.${pkgs.system}.default}/etc/dot.ghci";
+
+            ".stack/config.yaml".source =
+              "${self.packages.${pkgs.system}.default}/etc/dot.stack.config.yaml";
+          };
+        };
+      };
+    };
+}
